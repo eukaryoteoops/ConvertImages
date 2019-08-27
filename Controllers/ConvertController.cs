fa79284e477d8a63a932c7ec37536b4e1887cfce
TonyChang <tony.chang@phoenixnet.com.tw>
2019-08-27 17:01:23 +0800

    update

--- Controllers/ConvertController.cs
+++ Controllers/ConvertController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,48 +10,82 @@ using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
 using Files = System.IO.File;
 
-namespace ConvertImages.Controllers {
-    [Route ("api/[controller]")]
+namespace ConvertImages.Controllers
+{
+    [Route("api/[controller]")]
     [ApiController]
-    public class ConvertController : ControllerBase {
+    public class ConvertController : ControllerBase
+    {
+        private readonly HashSet<string> _failList;
+        public ConvertController()
+        {
+            _failList = new HashSet<string>();
+        }
+
         [HttpPost]
-        public async ValueTask<ActionResult> Post ([FromBody] HashSet<string> list) {
-            var failList = new HashSet<string> ();
-            foreach (var imgName in list) {
-                var imgBasePath = $"{Directory.GetCurrentDirectory ()}/images/{imgName}/{imgName}";
+        public async ValueTask<IActionResult> Post([FromBody] HashSet<string> list)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            foreach (var imgName in list)
+            {
+                var imgBasePath = $"{Directory.GetCurrentDirectory()}/images/{imgName}/{imgName}";
                 var horizonSize = $"{imgBasePath}";
-                await SaveJsonFile (horizonSize, failList);
+                await SaveJsonFile(horizonSize);
 
                 var BigSize = $"{imgBasePath}m";
-                await SaveJsonFile (BigSize, failList);
+                await SaveJsonFile(BigSize);
 
                 var smallSize = $"{imgBasePath}m-s";
-                await ResizeAndSave (280, 188, BigSize, smallSize);
-                await SaveJsonFile (smallSize, failList);
+                ResizeAndSave(280, 188, BigSize, smallSize);
+                await SaveJsonFile(smallSize);
             }
-            return Ok (failList);
+            sw.Stop();
+            return _failList.Count == 0 ? Ok($"{DateTime.Now.ToShortDateString()} Total: {list.Count} Elapsed: {sw.ElapsedMilliseconds} ms") 
+                                        : Ok(_failList);
         }
 
-        private async ValueTask ResizeAndSave (int width, int height, string oriImgPath, string destImgPath) {
-            if (Files.Exists ($"{destImgPath}.jpg"))
+        private void ResizeAndSave(int width, int height, string oriImgPath, string destImgPath)
+        {
+            if (Files.Exists($"{destImgPath}.jpg"))
                 return;
-            using (Image<Rgba32> image = Image.Load ($"{oriImgPath}.jpg")) {
-                image.Mutate (x => x.Resize (width, height));
-                image.Save ($"{destImgPath}.jpg");
+            try
+            {
+                using (Image<Rgba32> image = Image.Load($"{oriImgPath}.jpg"))
+                {
+                    image.Mutate(x => x.Resize(width, height));
+                    image.Save($"{destImgPath}.jpg");
+                }
+            }
+            catch (Exception e)
+            {
+                _failList.Add(e.Message);
+                return;
             }
         }
 
-        private async ValueTask SaveJsonFile (string picName, HashSet<string> failList) {
-            if (!Files.Exists ($"{picName}.jpg")) {
-                failList.Add ($"{picName}.jpg does not exist.");
+        private async ValueTask SaveJsonFile(string picName)
+        {
+            if (!Files.Exists($"{picName}.jpg"))
+            {
+                _failList.Add($"{picName}.jpg does not exist.");
                 return;
             }
             var dataType = "data:image/jpeg;base64";
-            byte[] imgdata = await Files.ReadAllBytesAsync ($"{picName}.jpg");
-            string base64Str = Convert.ToBase64String (imgdata);
-            using (StreamWriter file = Files.CreateText ($"{picName}.json")) {
-                JsonSerializer serializer = new JsonSerializer ();
-                serializer.Serialize (file, new { type = dataType, data = base64Str });
+            byte[] imgdata = await Files.ReadAllBytesAsync($"{picName}.jpg");
+            string base64Str = Convert.ToBase64String(imgdata);
+            try
+            {
+                using (StreamWriter file = Files.CreateText($"{picName}.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, new { type = dataType, data = base64Str });
+                }
+            }
+            catch (Exception e)
+            {
+                _failList.Add(e.Message);
+                return;
             }
         }
     }
